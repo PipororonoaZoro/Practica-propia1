cb381a770e0ee566fee47281974781f30ee3e518
PipororonoaZoro <maciel_ponce_9989@outlook.com.ar>
2026-03-26 20:36:57 -0300

    cambios

---
+++ src/config/dbConfig.mjs
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+export async function connectDB() {
+    try 
+    {
+        await mongoose.connect('mongodb+srv://macielponce9989_db_user:boca1324@clustercatamarca.jmgfbbx.mongodb.net/?appName=ClusterCatamarca');
+        console.log('Conexión exitosa a MongoDB');
+    }
+    catch (error)
+    {
+        console.error('Error al connectar a MongoDB:', error);
+        process.exit(1);
+    }
+}
